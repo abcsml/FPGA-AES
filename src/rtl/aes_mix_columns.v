c27e43bad9135c54c18491732bdaaff7c12251a3
abcsml <a338383154@163.com>
2023-04-17 09:17:46 +0800

    file: fix bug, pass test

--- src/rtl/aes_mix_columns.v
+++ src/rtl/aes_mix_columns.v
@@ -4,15 +4,15 @@ module aes_mix_columns (
 );
 
 function [ 7:0] aes_mul;
-    input [ 7:0] mix;
-    input [ 7:0] state;
+    input [ 3:0] mix;
+    input [ 7:0] y;
     reg   [ 7:0] a1,a2,a3;
     begin
-        a1 = state[7] ? (state << 1)^'h1B : state << 1;
+        a1 = y[7] ? (y << 1)^'h1B : y << 1;
         a2 = a1[7] ? (a1 << 1)^'h1B : a1 << 1;
         a3 = a2[7] ? (a2 << 1)^'h1B : a2 << 1;
 
-        aes_mul = (mix[0] ? state : 0)^
+        aes_mul = (mix[0] ? y : 0)^
                   (mix[1] ? a1 : 0)^
                   (mix[2] ? a2 : 0)^
                   (mix[3] ? a3 : 0);
